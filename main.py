d32b2402f3b0b9dcb505b5fdfbbc4124dcd46469
Antoni <alwasiak@student.agh.edu.pl>
2023-05-17 09:28:01 +0200

    a

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
-
+from typing import Union
+from pydantic import BaseModel
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+class ItStudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
